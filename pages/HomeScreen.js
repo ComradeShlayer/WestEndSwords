94c1f2105aac0377861614993ba622adca91ee22
Max Shlayer <maximshlayer@gmail.com>
2023-07-11 16:19:43 -0400

    - added win conditions - added changing scores - added reset function - change end timer to pause so that when a new timer starts you don't need to double tap - figured out colour stuff

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -2,7 +2,7 @@
 // https://aboutreact.com/react-native-bottom-navigation/
 import * as React from 'react';
 import { View, Text, SafeAreaView, Button, StyleSheet, Image, Pressable} from 'react-native';
-import { IconButton, Provider as PaperProvider } from 'react-native-paper';
+import { IconButton, Provider, Colors} from 'react-native-paper';
 // import { TextInput } from 'react-native-gesture-handler';
 import GoToButton from '../components/GoToButton'
 import StopwatchTimer, {
@@ -18,11 +18,47 @@ const HomeScreen = () => {
   const [finished, setFinish] = React.useState(false);
   const [leftScore, setLeft] = React.useState(0);
   const [rightScore, setRight] = React.useState(0);
-  const [timerLength, setLength] = React.useState(5000);
+  const [timerLength, setLength] = React.useState(180000);
+  const [win, setWin] = React.useState(false);
 
+  function checkScores(){
+    if(timerLength == 60000 && (rightScore == 4 || leftScore == 4)){
+      stopwatchRef.current?.pause()
+      setWin(true)
+    }
+    else if(timerLength == 180000 && (rightScore == 14 || leftScore == 14)){
+      stopwatchRef.current?.pause()
+      setWin(true)
+    }
+  }
+  
+  function changeRight(change){
+    if( (change == -1 && rightScore == 0)||(change == 1 && rightScore == 15) ){}
+    else{
+      setRight(rightScore + change);
+      checkScores();
+    }
+  }
+
+  function changeLeft(change){
+    if( (change == -1 && leftScore == 0)|| (change == 1 && leftScore == 15)){}
+    else{
+      setLeft(leftScore + change);
+      checkScores();
+    }
+  }
+ 
+  function reset(){
+    setWin(false);
+    setLeft(0);
+    setRight(0);
+    setFinish(false);
+    stopwatchRef.current?.reset()
+  }
 
   function endTimer(){
     setFinish(true);
+    stopwatchRef.current?.pause()
   }
 
   function timerFinished(){
@@ -84,7 +120,7 @@ const HomeScreen = () => {
             icon="refresh"
             mode="contained"
             size={32}
-            onPress={() => stopwatchRef.current?.reset()}
+            onPress={() => reset()}
           />
           <IconButton
             icon="clock-time-three"
@@ -116,6 +152,52 @@ const HomeScreen = () => {
             size={32}
             onPress={() => stopwatchRef.current?.reset()}
           /> */}
+        </View>
+        <View style={styles.scoreControlContainer}>
+          <IconButton
+              icon="arrow-up-drop-circle"
+              mode="contained"
+              // animated={false}
+              iconColor="#e80505"
+              backgroundColor="#ffd7d4"
+              // color="#144958"
+              style={styles.red}
+              size={32}
+              onPress={() => changeLeft(1)}
+          />
+          <IconButton
+            icon="arrow-up-drop-circle"
+            mode="contained"
+            iconColor="#00b03b"
+            backgroundColor="#bdfcd5"
+            size={32}
+            onPress={() => changeRight(1)}
+          />
+        </View>
+        <View style={styles.scoreContainer}>
+            <Text style={styles.scoreLeft}>{leftScore}</Text>
+            <Text style={styles.scoreRight}>{rightScore}</Text>
+          </View>
+          <View style={styles.scoreControlContainer}>
+          <IconButton
+              icon="arrow-down-drop-circle"
+              mode="contained"
+              // animated={false}
+              iconColor="#e80505"
+              backgroundColor="#ffd7d4"
+              // color="#144958"
+              // style={styles.red}
+              size={32}
+              onPress={() => changeLeft(-1)}
+          />
+          <IconButton
+            icon="arrow-down-drop-circle"
+            mode="contained"
+            iconColor="#00b03b"
+            backgroundColor="#bdfcd5"
+            size={32}
+            onPress={() => changeRight(-1)}
+          />
         </View>
       {/* <Text>Home Screen</Text>
       <Button
@@ -178,10 +260,41 @@ const styles = StyleSheet.create({
     width: 240,
     paddingTop: 48,
   },
+  scoreContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 240,
+    paddingTop: 0,
+    paddingHorizontal: 0,
+  },
+  scoreControlContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 270,
+    paddingTop: 0,
+    paddingHorizontal: 0,
+  },
   stopWatchChar: {
     fontSize: 48,
     fontWeight: 'bold',
     letterSpacing: 1,
     color: '#9CCC65',
+  },
+  scoreLeft: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    letterSpacing: 1,
+    color: '#e80505',
+  },
+  red: {
+    borderColor: '#e80505',
+    borderWidth: 0,
+    color: 'red',
+  },
+  scoreRight: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    letterSpacing: 1,
+    color: '#00b03b',
   },
 })
